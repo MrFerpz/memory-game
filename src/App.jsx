0b5622eb7cd8003698c6730eda230f1602738cb6
Fred <leferpz@gmail.com>
2025-01-06 07:54:47 -0500

    Added 9 more pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,15 @@ function App() {
       <Header score={score}/>
       <div class="main-grid">
         <Card pokemonName="ditto"></Card>
+        <Card pokemonName="groudon"></Card>
+        <Card pokemonName="suicune"></Card>
+        <Card pokemonName="bulbasaur"></Card>
+        <Card pokemonName="charmander"></Card>
+        <Card pokemonName="squirtle"></Card>
+        <Card pokemonName="pikachu"></Card>
+        <Card pokemonName="kyogre"></Card>
+        <Card pokemonName="rayquaza"></Card>
+        <Card pokemonName="blaziken"></Card>
       </div>
     </div>
   )
