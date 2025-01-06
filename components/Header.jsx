a8c265d075ca69afb293df140b8c14233b482f2f
Fred <leferpz@gmail.com>
2025-01-06 07:03:49 -0500

    Added header

---
+++ components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header({score}) {
+    return (
+        <div class = "header">
+                <h1>Memory Game</h1>
+                <div>Current Score: {score}</div>
+         </div>
+    )
+}
